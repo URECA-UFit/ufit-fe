fbb6df77faf1cb622cf655eec41c853e14f19f00
JIYEON HWANG <ghkdwldus0807@gmail.com>
2025-06-12 21:21:32 +0900

    fix: eslint 에러 해결결

--- src/views/PlanDetailPage.vue
+++ src/views/PlanDetailPage.vue
@@ -68,11 +68,11 @@
 
 <script setup>
 import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
-import axios from "axios";
+// import { useRoute } from "vue-router";
+// import axios from "axios";
 import ChatbotReviewModal from "@/components/ChatbotReviewModal.vue";
 
-const route = useRoute();
+// const route = useRoute();
 
 // 예시 하드코딩 데이터
 const examplePlan = {
